b8f78f760ff7559d327c456292e819f6c2404404
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2021-01-29 19:30:16 -0600

    basic client infastructure setup

---
+++ MyDewey/client/src/components/ApplicationViews.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { UserProfileContext } from "../providers/UserProfileProvider";
+import Login from "./Login";
+import Register from "./Register";
+import HiThere from "./HiThere";
+
+export default function ApplicationViews() {
+    const { isLoggedIn } = useContext(UserProfileContext);
+
+    return (
+        <main>
+            <Switch>
+
+                {/* Auth */}
+
+                <Route path="/login">
+                    <Login />
+                </Route>
+
+                <Route path="/register">
+                    <Register />
+                </Route>
+
+                <Route path="/unauthorized" exact>
+                    {isLoggedIn ? <HiThere /> : <Redirect to="/login" />}
+                </Route>
+            </Switch>
+        </main>
+    );
+};
---
+++ MyDewey/client/src/components/Register.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from "react";
+import { Button, Form, FormGroup, Label, Input, Card, CardBody } from 'reactstrap';
+import { useHistory } from "react-router-dom";
+import { UserProfileContext } from "../providers/UserProfileProvider";
+
+export default function Register() {
+    const history = useHistory();
+    const { register } = useContext(UserProfileContext);
+
+    const [firstname, setFirstName] = useState();
+    const [lastname, setLastName] = useState();
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+    //TODO:
+    //ADD REMAINING REGISTRATION FIELDS...
+
+    const registerClick = (e) => {
+        e.preventDefault();
+        if (password && password !== confirmPassword) {
+            alert("Passwords don't match. Please try again.");
+        } else {
+            const userProfile = { firstname, lastname, username, email };
+            register(userProfile, password)
+                .then(() => history.push("/"));
+        }
+    };
+
+    return (
+        <div className="container pt-4">
+            <div className="row justify-content-center">
+                <Card className="col-sm-12 col-lg-6">
+                    <CardBody>
+
+
+                        <Form onSubmit={registerClick}>
+                            <fieldset>
+                                <FormGroup>
+                                    <Label htmlFor="firstname">First Name</Label>
+                                    <Input id="firstname" type="text" onChange={e => setFirstName(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label htmlFor="lastname">Last Name</Label>
+                                    <Input id="lastname" type="text" onChange={e => setLastName(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input id="username" type="text" onChange={e => setUsername(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="email">Email</Label>
+                                    <Input id="email" type="text" onChange={e => setEmail(e.target.value)} />
+                                </FormGroup>
+                                {/* TODO:
+                                add remaining registration fields */}
+                                <FormGroup>
+                                    <Label for="password">Password</Label>
+                                    <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="confirmPassword">Confirm Password</Label>
+                                    <Input id="confirmPassword" type="password" onChange={e => setConfirmPassword(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Button>Register</Button>
+                                </FormGroup>
+                            </fieldset>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </div>
+        </div>
+    );
+}
