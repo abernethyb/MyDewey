f6b5f12c4841c4fbe62fd5b638ae53ff98d0448a
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2021-02-03 12:38:52 -0600

    added checkout methods to IItemRepository

--- MyDewey/Repositories/IItemRepository.cs
+++ MyDewey/Repositories/IItemRepository.cs
@@ -6,9 +6,14 @@ namespace MyDewey.Repositories
     public interface IItemRepository
     {
         void Add(Item item);
+        void ApproveCheckout(Checkout checkout);
+        void Checkin(Checkout checkout);
+        void DeclineCheckout(Checkout checkout);
         List<Item> GetAllItems();
+        List<Request> GetCheckoutRequests(int userProfileId);
         List<Item> GetNonUserItems(int userProfileId);
         List<Item> GetUserItems(int userProfileId);
         void RequestCheckout(int userProfileId, int itemId);
+        void VerifyCheckin(Checkout checkout);
     }
 }
